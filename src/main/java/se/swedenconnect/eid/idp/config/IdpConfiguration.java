a0d69c22ddf721a3e86d05fb560e503342977152
Martin Lindström <martin.lindstrom@litsec.se>
2026-03-10 17:48:41 +0100

    2.2.0

--- src/main/java/se/swedenconnect/eid/idp/config/IdpConfiguration.java
+++ src/main/java/se/swedenconnect/eid/idp/config/IdpConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 Sweden Connect
+ * Copyright 2016-2026 Sweden Connect
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ package se.swedenconnect.eid.idp.config;
 
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
